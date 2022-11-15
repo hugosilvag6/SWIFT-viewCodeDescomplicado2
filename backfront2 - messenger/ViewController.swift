600b94b6b20c65a70300ea0f9df34d85ada75baa
hugosilvag6 <hugosilvag6@hotmail.com>
2022-11-15 20:32:56 -0300

    Initial Commit

---
+++ backfront2 - messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  backfront2 - messenger
+//
+//  Created by Hugo Silva on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
